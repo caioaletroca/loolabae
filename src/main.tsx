77809fa80d4d0ed2aac7e365e7a5cc0b2a0ce8c0
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-08-29 22:16:15 -0300

    React router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { IntlProvider } from 'react-intl'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import HomePage from './pages/HomePage/index.tsx'
+import RootLayout from './pages/root.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <IntlProvider messages={{}} locale='en' defaultLocale='en'>
+      <RouterProvider router={router} />
+    </IntlProvider>
   </React.StrictMode>,
 )
