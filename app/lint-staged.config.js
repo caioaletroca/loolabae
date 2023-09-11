baab67de8f1b0c60e46203e9218e65ce56b0f237
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-09-11 20:09:35 -0300

    Prettier

--- app/lint-staged.config.js
+++ app/lint-staged.config.js
@@ -1,14 +1,4 @@
 /* eslint-disable no-undef */
-// import path from 'path';
-
-// const buildEslintCommand = (filenames) =>
-// 	`eslint --file ${filenames
-// 		.map((f) => path.relative(process.cwd(), f))
-// 		.join(' --file ')}`;
-
-// const stage = (filenames) =>
-// 	`git add ${filenames.map((f) => path.relative(process.cwd(), f)).join('')}`;
-
 export default {
 	'*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
 };
